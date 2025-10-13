7e61f3d576d6aae7415731658c5143b02e628e08
Antoine FONDEUR <antoine.fondeur@gmail.com>
2025-10-13 10:55:42 +0200

    refacto: add m31 and cm31 chip to verifier chip

--- verifier/verifier.go
+++ verifier/verifier.go
@@ -9,15 +9,21 @@ import (
 type VerifierChip struct {
 	api         frontend.API `gnark:"-"`
 	blake2sChip *blake2s.Blake2sChip
+	m31Chip     *m31.M31Chip
+	cm31Chip    *m31.CM31Chip
 	qm31Chip    *m31.QM31Chip
 }
 
 func NewVerifierChip(api frontend.API) *VerifierChip {
 	blake2sChip := blake2s.NewBlake2sChip(api)
-	qm31Chip := m31.NewQM31Chip(m31.NewCM31Chip(m31.NewM31Chip(api)))
+	m31Chip := m31.NewM31Chip(api)
+	cm31Chip := m31.NewCM31Chip(m31Chip)
+	qm31Chip := m31.NewQM31Chip(cm31Chip)
 	return &VerifierChip{
 		api:         api,
 		blake2sChip: blake2sChip,
+		m31Chip:     m31Chip,
+		cm31Chip:    cm31Chip,
 		qm31Chip:    qm31Chip,
 	}
 }
